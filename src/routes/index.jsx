d4a09ce91049327600553677f40a945f906c4dfe
ZainAmjad68 <zainamjad68@yahoo.com>
2023-06-05 12:58:53 +0500

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is a practice project by Zain Amjad to learn React Router.
+        <br />
+        Check out{" "}
+        <a href="https://github.com/ZainAmjad68/ai-language-buddy">
+          the main project - AI Language Buddy - that this relates to
+        </a>
+        .
+      </p>
+    );
+}
